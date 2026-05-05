96004484040b9ec2c052e129994335c9c38deaf9
SauRavRwT <souravrawat142@gmail.com>
2026-05-05 12:24:12 +0530

    switch: to vite.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
+const env = import.meta.env;
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.VITE_APP_FIREBASE_DATABASE_URL,
+  projectId: env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.VITE_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
